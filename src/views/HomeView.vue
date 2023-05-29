d5eed7acb31b35da110af41e1f1d76c97f7215c3
Patrick Scheich <scheichpatrick@gmail.com>
2023-05-29 16:02:24 +0200

    feature: Add Algorithm to find available people

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import FileUpload from '@/components/FileUpload.vue'
-import { ref } from 'vue';
+import { pushScopeId, ref } from 'vue';
 import * as Excel from 'exceljs';
 import SelectInput from '@/components/SelectInput.vue';
 
 const excelFileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
-let workbook = ref<Excel.Workbook>();
-let worksheets = ref<string[]>([]);
-let selectedWorksheet =  ref<string | null>(null);
+const workbook = ref<Excel.Workbook>();
+const worksheets = ref<string[]>([]);
+const selectedWorksheet =  ref<string | null>(null);
+const dateObj = ref<Date>(new Date());
+const people = ref<string[]>([])
+
+  
+const dateOptions: Intl.DateTimeFormatOptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  };
+
 
 const handleFileSelected = (file: File) => {
   const reader = new FileReader();
@@ -23,8 +33,6 @@ const handleFileSelected = (file: File) => {
 
       worksheets.value = wb.worksheets.map(elem => elem.name)
       selectedWorksheet.value = worksheets.value[0]
-
-      console.log(worksheets)
     }).catch((error) => {
       console.error('Fehler beim Einlesen der Datei:', error);
     });
@@ -34,13 +42,13 @@ const handleFileSelected = (file: File) => {
 };
 
 
-function findeZelleMitParameter(parameter: string): Excel.Cell | null {
+function findCellWithParameter(parameter: string): Excel.Cell | null {
   let worksheetname:string = ''
   if (selectedWorksheet.value)
     worksheetname= selectedWorksheet.value;
   if (!workbook.value)
     return null;
-    const worksheet = workbook.value.getWorksheet(worksheetname);
+  const worksheet = workbook.value.getWorksheet(worksheetname);
 
   const numberOfRows = worksheet.rowCount
   const rows = worksheet.getRows(0, numberOfRows) ?? [];
@@ -53,26 +61,68 @@ function findeZelleMitParameter(parameter: string): Excel.Cell | null {
       // Überprüfe, ob der Zellenwert mit dem Parameter übereinstimmt
       const cell = row.getCell(index)
       if (cell.value === parameter) {
-        console.log(cell.value)
-        console.log(cell.row)
-
-        const datestring = worksheet.getCell("AJ7").value
-        const dateString = datestring?.toLocaleString()
-        if(!dateString)
-          return null;
-        const date = new Date(dateString)
-        const options: Intl.DateTimeFormatOptions = {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric'
-        };
-        console.log(date.toLocaleDateString('de-DE', options))
         return cell;
       }
     }
   }
 
   return null; // Zelle nicht gefunden
+}
+
+
+const getDate = () => {
+  const date = new Date(dateObj.value)
+  return date.toLocaleString('de-DE', dateOptions)
+}
+
+const getPeople = () => {
+  const cellWithName = findCellWithParameter('Name')
+  const cellWithForname = findCellWithParameter('Vorname')
+
+  let worksheetname:string = ''
+  if (selectedWorksheet.value)
+    worksheetname= selectedWorksheet.value;
+  if (!workbook.value)
+    return null;
+  const worksheet = workbook.value.getWorksheet(worksheetname);
+
+  if (! cellWithName || !cellWithForname)
+    return;
+
+  
+  let cellWithDate: Excel.Cell = worksheet.getCell('A1')
+  const rowNumber = parseInt(cellWithName.row)
+  const rows = worksheet.getRow(rowNumber)
+  const cellCount = rows.cellCount
+
+  for (let index = 1; index <= cellCount; index++) {
+    const text = rows.getCell(index).value?.toString()
+    if (!text)
+      return
+    const date = new Date(text)
+    if (date.toLocaleString('de-DE', dateOptions) === getDate())
+      cellWithDate = rows.getCell(index)
+  }
+
+
+  const columnName = worksheet.getColumn(cellWithName.col)
+  console.log(columnName.values.length)
+  const startindex = parseInt(cellWithName.row)
+
+  // const leastLength = Math.min(...[columbName.values.length, columnForname.values.length, columnDate.values.length])
+  for (let index = startindex; index <= columnName.values.length; index ++){
+    if (worksheet.getCell(index, cellWithDate.col).value?.toString() === undefined){
+      
+      const forname = worksheet.getCell(index, cellWithForname.col).value?.toString()
+      const name = worksheet.getCell(index, cellWithName.col).value?.toString()
+
+      if (!forname || !name || forname === name)
+        continue
+
+      console.log(forname.concat(' ', name))
+      people.value.push(forname.concat(' ', name))
+    }
+  }
 }
 </script>
 
@@ -86,7 +136,10 @@ function findeZelleMitParameter(parameter: string): Excel.Cell | null {
     <h2>Worksheets</h2>
     <SelectInput :options="worksheets" @worksheet-selected="selectedWorksheet = $event"/>
     <p>Ausgewähltes Worksheet: {{ selectedWorksheet }}</p>
-    <button v-if="workbook && selectedWorksheet" style="width:100px; height: 50px" @click="findeZelleMitParameter('Name')">Fire</button>
+    <button v-if="workbook && selectedWorksheet" style="width:100px; height: 50px" @click="getPeople()">Fire</button>
+    <input type="date" name="" id="" v-model="dateObj"  pattern="\d{2}.\d{2}.\d{4}">
+    <p>{{ getDate() }}</p>
+    <p>{{people}}</p>
   </main>
 </template>
 
